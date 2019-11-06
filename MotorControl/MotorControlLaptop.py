6a270a2467c290ace4fb29b775122539ead63b92
Cody Black <codyblack@ou.edu>
2019-11-05 20:30:42 -0600

    Laptop is now UDP server instead of TCP client

--- MotorControl/MotorControlLaptop.py
+++ MotorControl/MotorControlLaptop.py
@@ -1,35 +1,29 @@
-import pygame, sys, serial, json, os, socketserver
-from time import sleep
-from enum import Enum
+import pygame, sys, serial, json, socketserver
 
-class Handler_TCPServer(socketserver.BaseRequestHandler):
-    """
-    The TCP Server class for demonstration.
-
-    Note: We need to implement the Handle method to exchange data
-    with TCP client.
-
-    """
-
+class Handler_UDPServer(socketserver.BaseRequestHandler):
     def handle(self):
-        # self.request - TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request[0].strip()
+        print(self.data.decode())
+        # Get button/joystick values
+        pygame.event.get()
+        # Turn on LED if A is pressed   
         if joystick.get_button(0) == 1:
             dataSend["led"] = True
+        # Turn off LED if B is pressed
         elif joystick.get_button(1) == 1:
             dataSend["led"] = False
-
         # Read y-positions of left and right sticks
         dataSend["leftStick"] = joystick.get_axis(1)
         dataSend["rightStick"] = joystick.get_axis(3)
 
         # If y-pos of sticks are in "dead zone", round them to 0
+        # TODO: pygame can probably do this for us
         if abs(dataSend["leftStick"]) < DEAD_ZONE_Y:
             dataSend["leftStick"] = 0
         if abs(dataSend["rightStick"]) < DEAD_ZONE_Y:
             dataSend["rightStick"] = 0
-        
-        self.request.sendall(json.dumps(dataSend).encode())
+
+        self.request[1].sendto(json.dumps(dataSend).encode(), self.client_address)
 
 # Dead zone for y-axis
 DEAD_ZONE_Y = 0.1
@@ -37,21 +31,22 @@ DEAD_ZONE_Y = 0.1
 #setup
 pygame.display.init()
 pygame.init()
-#window = pygame.display.set_mode((200, 200))
-#pygame.display.set_caption("Xbox Controller Testing")
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 dataSend = {}
+
 dataSend["led"] = True
 dataSend["leftStick"] = 0.0
 dataSend["rightStick"] = 0.0
 
 if __name__ == "__main__":
+    # TODO: figure out a better way to find the host ip
     HOST, PORT = "192.168.1.52", 9999
-    socketserver.TCPServer.allow_reuse_address = True
-    # Init the TCP server object, bind it to the localhost on 9999 port
-    tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
+    # TODO: do we still need this? leftover from switch from TCP to UDP
+    socketserver.UDPServer.allow_reuse_address = True
+    # Init the UDP server object, bind it to the localhost on 9999 port
+    udp_server = socketserver.UDPServer((HOST, PORT), Handler_UDPServer)
 
-    # Activate the TCP server.
-    # To abort the TCP server, press Ctrl-C.
-    tcp_server.serve_forever()
+    # Activate the UDP server.
+    # To abort the UDP server, press Ctrl-C.
+    udp_server.serve_forever()
