058f76b8e59dacbf2f03ee301cdf4ab439473dcd
Cody Black <codyblack@ou.edu>
2019-11-05 18:54:04 -0600

    Switched server and client

--- MotorControl/MotorControlLaptop.py
+++ MotorControl/MotorControlLaptop.py
@@ -1,37 +1,38 @@
-import pygame, sys, serial, json, os
+import pygame, sys, serial, json, os, socketserver
 from time import sleep
 from enum import Enum
-import socket
 
-host_ip, server_port = "192.168.1.44", 9999
+class Handler_TCPServer(socketserver.BaseRequestHandler):
+    """
+    The TCP Server class for demonstration.
 
-# Initialize a TCP client socket using SOCK_STREAM
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Note: We need to implement the Handle method to exchange data
+    with TCP client.
+
+    """
+
+    def handle(self):
+        # self.request - TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        if joystick.get_button(0) == 1:
+            dataSend["led"] = True
+        elif joystick.get_button(1) == 1:
+            dataSend["led"] = False
+
+        # Read y-positions of left and right sticks
+        dataSend["leftStick"] = joystick.get_axis(1)
+        dataSend["rightStick"] = joystick.get_axis(3)
+
+        # If y-pos of sticks are in "dead zone", round them to 0
+        if abs(dataSend["leftStick"]) < DEAD_ZONE_Y:
+            dataSend["leftStick"] = 0
+        if abs(dataSend["rightStick"]) < DEAD_ZONE_Y:
+            dataSend["rightStick"] = 0
+        
+        self.request.sendall(json.dumps(dataSend).encode())
 
 # Dead zone for y-axis
 DEAD_ZONE_Y = 0.1
-
-# Because there is only 1 hat, it doesn't need an enum. Returns a coordinate (x,y)
-#Enum Class describing all of the Buttons
-class XboxButtons(Enum):
-    A = 0
-    B = 1
-    X = 2
-    Y = 3
-    LB = 4
-    RB = 5
-    BACK = 6
-    START = 7
-    LJOY = 8
-    RJOY = 9
-
-#Enum Class describing all of the axes
-class XboxAxes(Enum):
-    LY = 0
-    LX = 1
-    T = 2 #Analog triggers, right side negative - result is sum of them (-1, 1)
-    RY = 3
-    RX = 4
 
 #setup
 pygame.display.init()
@@ -40,55 +41,17 @@ pygame.init()
 #pygame.display.set_caption("Xbox Controller Testing")
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
-
 dataSend = {}
 dataSend["led"] = True
 dataSend["leftStick"] = 0.0
 dataSend["rightStick"] = 0.0
-done = False
-while (not done):
-    for event in pygame.event.get(): # User did something.
-        if event.type == pygame.QUIT: # If user clicked close.
-            done = True # Flag that we are done so we exit this loop.
-    '''
-    For debugging: prints pressed button(s)
-        elif event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        elif event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-    window.fill(pygame.Color('white'))
-    pressedButtons = []
-    for i in range(joystick.get_numbuttons()):
-        if joystick.get_button(i) == 1:
-            pressedButtons.append(XboxButtons(i))
-    for n in pressedButtons:
-        print(n.name + " ")
-    '''
-    if joystick.get_button(0) == 1:
-        dataSend["led"] = True
-    if joystick.get_button(1) == 1:
-        dataSend["led"] = False
 
-    # Read y-positions of left and right sticks
-    dataSend["leftStick"] = joystick.get_axis(1)
-    dataSend["rightStick"] = joystick.get_axis(3)
+if __name__ == "__main__":
+    HOST, PORT = "192.168.1.52", 9999
+    socketserver.TCPServer.allow_reuse_address = True
+    # Init the TCP server object, bind it to the localhost on 9999 port
+    tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
 
-    # If y-pos of sticks are in "dead zone", round them to 0
-    if abs(dataSend["leftStick"]) < DEAD_ZONE_Y:
-        dataSend["leftStick"] = 0
-    if abs(dataSend["rightStick"]) < DEAD_ZONE_Y:
-        dataSend["rightStick"] = 0
-    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # Establish connection to TCP server and exchange data
-        tcp_client.connect((host_ip, server_port))
-        tcp_client.sendall(json.dumps(dataSend).encode())
-
-        # Read data from the TCP server and close the connection
-        received = tcp_client.recv(1024)
-    finally:
-        tcp_client.close()
-
-    print ("Bytes Sent:     {}".format(dataSend))
-    print ("Bytes Received: {}".format(received.decode()))
-pygame.quit()
+    # Activate the TCP server.
+    # To abort the TCP server, press Ctrl-C.
+    tcp_server.serve_forever()
--- MotorControl/MotorControlPi.py
+++ MotorControl/MotorControlPi.py
@@ -1,44 +1,23 @@
-import pygame, sys, serial, json, os
+import pygame, sys, serial, json, os, socket
 from time import sleep
 from enum import Enum
-import socketserver
-
-class Handler_TCPServer(socketserver.BaseRequestHandler):
-    """
-    The TCP Server class for demonstration.
-
-    Note: We need to implement the Handle method to exchange data
-    with TCP client.
-
-    """
-
-    def handle(self):
-        # self.request - TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        # Send data to Arduino
-        ser.write(self.data)
-        # Read data from Arduino
-        dataRead = ser.readline()
-        # Make sure read data is valid JSON
-        try:
-            # Print data read from Arduino
-            print(json.loads(dataRead.decode("utf-8")))
-        except json.decoder.JSONDecodeError:
-            print("Error reading from Arduino")
-        # Send data from Arduino to client
-        self.request.sendall(dataRead)
+host_ip, server_port = "192.168.1.52", 9999
 
 ser = serial.Serial(timeout = 1) # Set timeout to 1 second
 ser.baudrate = 38400
 ser.port = "/dev/ttyUSB1" # TODO: figure out how to find the port Arduino is connected to automatically
 ser.open()
 
-if __name__ == "__main__":
-    HOST, PORT = "192.168.1.44", 9999
-    socketserver.TCPServer.allow_reuse_address = True
-    # Init the TCP server object, bind it to the localhost on 9999 port
-    tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
+while True:
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+       # Establish connection to TCP server and exchange data
+        tcp_client.connect((host_ip, server_port))
 
-    # Activate the TCP server.
-    # To abort the TCP server, press Ctrl-C.
-    tcp_server.serve_forever()
+        # Read data from the TCP server and close the connection
+        received = tcp_client.recv(1024)
+        ser.write(received)
+        dataRead = ser.readline()
+        tcp_client.sendall(dataRead)
+    finally:
+        tcp_client.close()
