8356bee05b83cf93273b25704ff65d57c59f4b87
medgar591 <matthewaedgar@gmail.com>
2019-11-06 18:13:29 -0600

    Update MotorControlPi.py

--- MotorControl/MotorControlPi.py
+++ MotorControl/MotorControlPi.py
@@ -1,4 +1,4 @@
-import sys, serial, json, socket
+import sys, serial, json, socket, cv2
 host_ip, server_port = "192.168.1.52", 9999
 
 ser = serial.Serial(timeout = 1) # Set serial timeout to 1 second
@@ -6,9 +6,23 @@ ser.baudrate = 38400
 ser.port = "/dev/ttyUSB0" # TODO: use try/catch to find the port Arduino is connected to automatically
 ser.open()
 
-dataRead = "Client start".encode()
-
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0) #Set this to 1 in order to use the webcamera if you have one built in
+
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    rval = False
+
+cv2.destroyWindow("preview")
+
+data = "Client start. Camera: " + str(rval)
+
+dataRead = data.encode()
+
+
 while True:
     # Try loop to prevent horrible issues should errors get thrown
     try:
