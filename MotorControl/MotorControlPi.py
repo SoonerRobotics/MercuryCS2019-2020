89597452323829de0d8776414b9616c207a6f33c
MercuryPi <a@b.com>
2020-01-16 18:55:50 -0600

    Removed old code that was commented out

--- MotorControl/MotorControlPi.py
+++ MotorControl/MotorControlPi.py
@@ -1,5 +1,4 @@
 import sys, serial, json, socket, datetime, time
-#import cv2
 host_ip, server_port = "192.168.1.52", 9999
 
 ser = serial.Serial(timeout = 1) # Set serial timeout to 1 second
@@ -8,65 +7,11 @@ ser.port = "/dev/ttyUSB0" # TODO: use try/catch to find the port Arduino is conn
 ser.open()
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-'''
-vc = cv2.VideoCapture(0) #Set this to 1 in order to use the webcamera if you have one built in
-
-if vc.isOpened():
-    rval, frame = vc.read()
-else:
-    rval = False
-
-data = str(datetime.datetime.now().time()) + ": Client start. Camera: " + str(rval)
-'''
-# dataRead = data.encode()
-'''
-#Retrieving the properties for the frame size
-width = vc.get(3)   # float
-height = vc.get(4) # float
-
-width = int(width)
-height = int(height)
-
-oldDimensions = (width, height) #For upscaling after downscaling, so that the size of the image appears constant
-
-scale_percent = 20 #Modify this to change the quality of the image, 100 equals no change
-width = width * scale_percent / 100
-height = height * scale_percent / 100
-
-width = int(width)
-height = int(height)
-
-newDimensions = (width, height)
-
-#Changing image quality
-downscale = cv2.resize(frame, newDimensions, interpolation = cv2.INTER_AREA)
-resized = cv2.resize(downscale, oldDimensions, interpolation = cv2.INTER_AREA)
-
-#Changing image color
-gray = cv2.cvtColor(downscale, cv2.COLOR_BGR2GRAY)
-
-img_str = cv2.imencode('.jpg', gray)[1]
-
-#udp_client.sendto(dataRead, (host_ip, server_port))
-print(len(img_str))
-udp_client.sendto(img_str, (host_ip, server_port))
-'''
-# udp_client.sendto(dataRead, (host_ip, server_port))
 
 dataRead = "".encode()
 
+# Main control loop
 while True:
-    # Get frame
-    # rval, frame = vc.read()
-    # Downscale to reduce size
-    # downscale = cv2.resize(frame, newDimensions, interpolation = cv2.INTER_AREA)
-    # Make image grayscale to reduce size
-    # gray = cv2.cvtColor(downscale, cv2.COLOR_BGR2GRAY)
-    # Encode frame
-    # rval, encodedFrame = cv2.imencode('.jpg', gray)
-    # Send encode frame to UDP server
-    # udp_client.sendto(encodedFrame, (host_ip, server_port))
-    # Try loop to prevent horrible issues should errors get thrown
     try:
         # Send data to UDP Server
         udp_client.sendto(dataRead, (host_ip, server_port))
