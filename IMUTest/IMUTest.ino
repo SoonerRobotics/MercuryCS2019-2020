4974b55b37a400e526dbf098dc16c06cd1a037f4
Cody <codyblack@ou.edu>
2020-01-30 18:57:40 -0600

    Turn to angle with timeout - needs tuning

--- IMUTest/IMUTest.ino
+++ IMUTest/IMUTest.ino
@@ -8,11 +8,19 @@ Motor rightMotor;
 
 float turnPower;
 
+unsigned long startTime;
+
+const float TURN_DEADZONE = 5.0;
+// Time to finish a turn in milliseconds
+const unsigned long TURN_FINISH_TIME = 5000;
+
+const float ANGLE_CHANGE = 90.0;
+
 // BNO055
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 sensors_event_t orientationData;
 
-// Heading control
+// Heading control  
 PIDController yawController;
 float targetAngle = 180.0;
 
@@ -45,6 +53,16 @@ float angleDiff(float a,float b)
     return dif - 180;
 }
 
+void turnToHeading(float currentHeading) {
+    // Achieve the current target heading by locking the IMU to the desired yaw
+    turnPower = -yawController.update(0, angleDiff(constrainAngle(orientationData.orientation.x), targetAngle));
+    //turnPower = RLUtil::clamp(turnPower, -1, 1);
+    Serial.print("Heading: " + String(orientationData.orientation.x));
+    Serial.println("    turnPower: " + String(turnPower));
+    leftMotor.output(-1 * turnPower);
+    rightMotor.output(turnPower);
+}
+
 void setup() {
     Serial.begin(9600);
 
@@ -57,8 +75,10 @@ void setup() {
     }
 
     delay(1000);
+    // Set to mode where magnetometer is turned off
+    bno.setMode(Adafruit_BNO055::OPERATION_MODE_IMUPLUS);
     // Set up the yaw PID controller
-    yawController.begin(0, 0.01,0.01, 0);
+    yawController.begin(0, 0.01, 0.01, 0);
     leftMotor.begin(8,9,5).reverse();
     rightMotor.begin(10,12,6).reverse();
     // Initialize loop timer
@@ -66,14 +86,23 @@ void setup() {
 }
 
 void loop() {
-    // BNO055 sensor data
+    // Get BNO055 orientation data
     bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-    // Achieve the current target heading by locking the IMU to the desired yaw
-    turnPower = -yawController.update(0, angleDiff(constrainAngle(orientationData.orientation.x), targetAngle));
-    turnPower = RLUtil::clamp(turnPower, -1, 1);
-    Serial.print(orientationData.orientation.x);
-    Serial.print("     ");
-    Serial.println(turnPower);
-    leftMotor.output(-1 * turnPower);
-    rightMotor.output(turnPower);
+    // The robot is not close to the desired heading
+    if ((fabs(angleDiff(constrainAngle(orientationData.orientation.x), targetAngle)) - TURN_DEADZONE) > 0) {
+      turnToHeading(orientationData.orientation.x);
+    }  
+    // The robot is close enough to the desired heading
+    else {
+      startTime = millis();
+      while ((millis() - startTime) < TURN_FINISH_TIME) {
+        bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+        turnToHeading(orientationData.orientation.x);
+      }
+      leftMotor.output(0.0);
+      rightMotor.output(0.0);
+      delay(3000);
+      targetAngle = constrainAngle(targetAngle + ANGLE_CHANGE);
+      Serial.println("New target heading: " + String(targetAngle));
+    }
 }
