2cfeb41528ba5bf83b718e550a0dc76e56bfb9eb
Will Spaeth <jwspaeth0@gmail.com>
2020-02-07 10:33:14 -0600

    Code for ui transmitter with ros is complete. Not tested

--- mercury_ws/src/mercury_nav/src/ui_transmitter_node.py
+++ mercury_ws/src/mercury_nav/src/ui_transmitter_node.py
@@ -6,17 +6,11 @@ import datetime
 import time
 
 import rospy
+import cv2
 import imutils
 from imutils.video import VideoStream
 
 from mercury_msgs.msg import sensors
-
-# This depends on the IP of the device running the UDP server (the driver's device)
-host_ip = "192.168.1.74" # IP of laptop using UI software
-server_port = 9000
-
-# How often to send data to UI
-hz = 15
 
 # Sensor values updated by callback
 sensor_values = sensors()
@@ -26,24 +20,57 @@ vs = VideoStream(src=0).start()
 
 def send_tcp():
 
-    global sensor_values
+    # How often this function repeats in hertz
+    hz = 15
+
+    # The IP address of the UI server and the port that the UI receive is running
+    host_ip = "192.168.1.74"
+    server_port = 9000 # Don't change this value
     
     rospy.init_node("ui_transmitter_node")
     rospy.loginfo("UI Transmitter Node Started")
     rospy.Subscriber("serial_sensor_receiver_node", sensors, sensor_update_callback)
-    
-    while not rospy.is_shutdown():
 
-        sensor_dict = {}
-        sensor_dict["frame"] = fetch_frame()
-        sensor_dict["us_front"] = sensor_values.us_front
-        sensor_dict["us_left"] = sensor_values.us_left
-        sensor_dict["us_right"] = sensor_values.us_right
-        sensor_dict["mag_x"] = sensor_values.mag_x
-        sensor_dict["mag_y"] = sensor_values.mag_y
-        sensor_dict["mag_z"] = sensor_values.mag_z
+    rospy.loginfo("Trying to connect to server {}:{}".format(host_ip, server_port))
+    while True:
+        try:
+            tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp_client.connect((host_ip, server_port))
+            c_status = "Connected"
+            rospy.loginfo(c_status)
+        except Exception as e:
+            rospy.loginfo("Error: {}".format(e))
+
+            # While connected, write sensors to server
+        while c_status == "Connected":
+
+            send_dict = get_send_dict()
+
+            send_msg = json.dumps(send_dict) + "\n"
+            send_msg = send_msg.encode()
+
+            try:
+                tcp_client.sendall(send_msg)
+            except Exception as e:
+                c_status = "Disconnected"
+                rospy.loginfo("Error: {}".format(e))
         
         time.sleep(1/hz)
+
+def get_send_dict():
+
+    global sensor_values
+
+    sensor_dict = {}
+    sensor_dict["frame"] = fetch_frame().tolist()
+    sensor_dict["us_front"] = sensor_values.us_front
+    sensor_dict["us_left"] = sensor_values.us_left
+    sensor_dict["us_right"] = sensor_values.us_right
+    sensor_dict["mag_x"] = sensor_values.mag_x
+    sensor_dict["mag_y"] = sensor_values.mag_y
+    sensor_dict["mag_z"] = sensor_values.mag_z
+
+    return sensor_dict
 
 def send_dict(sensor_dict):
     pass
