18cc69e66ae55d0cbb0fe3c67cb0f5fcf87bdd0c
Matt Edgar <Matthew.A.Edgar-1@ou.edu>
2020-02-06 18:37:18 -0600

    Added to code, moved file

--- TestCode/Testing/slow_ultrasonic.ino
+++ TestCode/Testing/slow_ultrasonic.ino
@@ -1,21 +1,42 @@
 //Heavily based on the wall_avoid.ino code, but modified to function with an ultrasonic sensor which requires a ping to operate
 #include <RobotLib.h>
 
+//New variables:
+const int ECHO_PIN = D4; //Change as necessary
+const int TRIG_PIN = D3; //Change as necessary
+
+//Copied variables:
+const int TURN_TIME = 500;
+// How fast the wheel motors spin, from 0 to 1
+const double DRIVE_SPEED = 0.4;
+// Distance from the wall at which the robot slows down
+const double SLOW_DISTANCE = 30.0;
+// Distance from the wall at which the robot starts to turn
+const double TURN_DISTANCE = 15.0;
 
 Motor motorLeft;
 Motor motorRight;
+double distance;
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(DIST_PIN, INPUT);
+  pinMode(ECHO_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
   motorLeft.begin(8,9,5).reverse();
   motorRight.begin(10,12,6).reverse();
 }
 
 void loop() {
   //Code for determining distance with the sensor
+  sum = 0;
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  //Wait for echo
+  Serial.println(pulseIn(ECHO_PIN, HIGH)/148.0
+
 
   //Following code straight copied:
   // If we're very close to a wall
