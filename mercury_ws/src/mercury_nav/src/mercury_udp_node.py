6398556bf4b1578bd1d08bbfb027225983e0a9a0
Cody <codyblack@ou.edu>
2020-02-06 18:54:50 -0600

    Made node send data so it can receive data from server

--- mercury_ws/src/mercury_nav/src/mercury_udp_node.py
+++ mercury_ws/src/mercury_nav/src/mercury_udp_node.py
@@ -5,27 +5,32 @@ import socket
 import json
 from mercury_msgs.msg import motors
 
-host_ip = "10.8.0.2"
+# This depends on the IP of the device running the UDP server (the driver's device)
+host_ip = "10.8.0.4"
 server_port = 9999
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def receive_udp():
-    pub = rospy.Publisher("/mercury/motors", motors, queue_size=1)
     # Initialize ROS node
     rospy.init_node("mercury_udp_node")
+    rospy.loginfo("UDP Node Started")
+    pub = rospy.Publisher("/mercury/motors", motors, queue_size=1)
     motor_values = motors()
     while not rospy.is_shutdown():
+        # TODO: Send meaninful data back here
+        udp_client.sendto("", (host_ip, server_port))
         # Read data from the UDP server
         dataReceived = udp_client.recv(1024)
+        # rospy.loginfo(dataReceived)
         try:
             decodedData = dataRecevied.decode()
             data = json.loads(decodedData)
+            motor_values.left = data["leftStick"]
+            motor_values.right = data["rightStick"]
+            pub.publish(motor_values)
         except:
             pass
-        motor_values.left = data["leftStick"]
-        motor_values.right = data["rightStick"]
-        pub.publish(motor_values)
 
 if __name__ == "__main__":
     try:
