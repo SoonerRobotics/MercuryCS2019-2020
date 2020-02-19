3455dcf42be3e48717fcc3d3174ab1714d98063e
Matt Edgar <Matthew.A.Edgar-1@ou.edu>
2020-02-18 19:32:35 -0600

    Expanded code for conditions based on distances on sides of robot

--- FinalCode/Nodes/sensor_driver/sensor_driver.ino
+++ FinalCode/Nodes/sensor_driver/sensor_driver.ino
@@ -1,5 +1,6 @@
 #include <hcsr04.h> //Bifrost Library for HC SR04 sensors
 
+/* Declaring constants that will not be changing throughout the program */
 // First sensor, the nice one that goes on the front of the bot, analog because of the sensor
 const int S1_PIN = A0;
 const double NUM_SAMPLES = 20.0;
@@ -9,12 +10,15 @@ const int S2_ECHO = 10;
 // Third sensor, polling one that goes on the left side of the bot
 const int S3_TRIG = 9;
 const int S3_ECHO = 8;
+
 bool moving = false; //OMG, the bot should not start moving on startup
+const double SIDE_CLEARANCE = 75.0; //This double is the clearance needed on a side for the robot to rotate in place, given in Millimeters
 
-//Variables used other places, but not initialized here
+/*Variables used other places, but not initialized here*/
 int sum;
+bool bool1, bool2;
 
-// Declaring the side sensors
+/* Declaring the side sensors */
 // HCSR04 name(trigger pin, echo pin, minimum distance in mm, maximum distance in mm)
 HCSR04 SensorRight(S2_TRIG, S2_ECHO, 25, 4000);
 HCSR04 SensorLeft(S3_TRIG, S3_ECHO, 25, 4000);
@@ -31,8 +35,28 @@ void loop() {
     delay(250);
   }
   else {
-    // TODO: Add code to start moving / move differently if certain conditions are met
-    moving = true;
+    if (checkSensor1() > SIDE_CLEARANCE) {
+      // TODO: Replace condition for allowing the bot to move forward
+      // TODO: Add code to actually control motors on the bot
+      moving = true;
+    }
+    else {
+      bool1 = SensorRight.distanceInMillimeters() < SIDE_CLEARANCE;
+      bool2 = SensorLeft.distanceInMillimeters() < SIDE_CLEARANCE;
+
+      if (bool1 && bool2) {
+        // TODO: Include code to move backwards
+      }
+      else if (bool1 && !(bool2)) {
+        // TODO: Include code to move backwards and to the left
+      }
+      else if (bool2 && !(bool1)) { // order of bools switched to take advantage of short circuit execution
+        // TODO: Include code to move backwards and to the right
+      }
+      else {
+        // TODO: Include code to rotate in place
+      }
+    }
   }
 }
 
