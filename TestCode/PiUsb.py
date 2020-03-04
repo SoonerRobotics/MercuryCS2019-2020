77736c8d0001369cae80e61a3edf76e5aa76fb3e
Matt Edgar <Matthew.A.Edgar-1@ou.edu>
2020-03-03 19:05:17 -0600

    Removed extra comments

--- TestCode/PiUsb.py
+++ TestCode/PiUsb.py
@@ -23,35 +23,3 @@ for port in ports:
         pass
 
 print(result)
-
-"""
-def serial_ports():
-    \""" Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    \"""
-    if sys.platform.startswith('win'):
-        print("Windows detected")
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        print("Linux detected")
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-"""
