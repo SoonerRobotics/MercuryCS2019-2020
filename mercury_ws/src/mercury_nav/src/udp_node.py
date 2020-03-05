e5303152c90812b079d94c62e909ba844d52d9da
Ubuntu <codyblack@ou.edu>
2020-03-05 22:17:28 +0000

    Added timeout and error messages to UDP node

--- mercury_ws/src/mercury_nav/src/udp_node.py
+++ mercury_ws/src/mercury_nav/src/udp_node.py
@@ -6,10 +6,11 @@ import json
 from mercury_msgs.msg import motors
 
 # This depends on the IP of the device running the UDP server (the driver's device)
-host_ip = "10.8.0.4"
+host_ip = "10.8.0.2"
 server_port = 9999
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_client.settimeout(1) # Timeout after 1 second
 
 def receive_udp():
     # Initialize ROS node
@@ -19,19 +20,28 @@ def receive_udp():
     motor_values = motors()
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        # TODO: Send meaningful data back here
-        udp_client.sendto("", (host_ip, server_port))
-        # Read data from the UDP server
-        dataReceived = udp_client.recv(1024)
-        # rospy.loginfo(dataReceived)
         try:
-            decodedData = dataReceived.decode()
-            data = json.loads(decodedData)
-            motor_values.left = data["leftStick"]
-            motor_values.right = data["rightStick"]
-            pub.publish(motor_values)
-        except:
-            pass
+            # TODO: Send meaningful data back here
+            udp_client.sendto("", (host_ip, server_port))
+            # Read data from the UDP server
+            dataReceived = udp_client.recv(1024)
+            # rospy.loginfo(dataReceived)
+        except Exception as e:
+            rospy.loginfo("Error sending/receiving over UDP: %s", str(e))
+            # Error getting control data, so stop the motors
+            motor_values.left = 0
+            motor_values.right = 0
+        else:
+            try:
+                data = json.loads(dataReceived.decode())
+            except Exception as e:
+                rospy.loginfo(str(e))
+            else:
+                motor_values.left = data["leftStick"]
+                motor_values.right = data["rightStick"]
+
+        rospy.loginfo(motor_values)
+        pub.publish(motor_values)
         rate.sleep()
     #rospy.spin()
 
