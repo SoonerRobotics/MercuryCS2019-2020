e988f3938f9e7b72dcb714192172cbf41734441a
Cody Black <codyblack@ou.edu>
2020-03-05 14:19:45 -0600

    Added Matt's ultrasonic sensor code to SensorArduino code

--- SensorArduino/SensorArduino.ino
+++ SensorArduino/SensorArduino.ino
@@ -1,14 +1,29 @@
 #include <ArduinoJson.h>
+#include <hcsr04.h> //Bifrost Library for HC SR04 sensors
 
-// Enough space for 1 object with 6 members
+// Enough space for 1 object with 7 members
 StaticJsonDocument<JSON_OBJECT_SIZE(7)> jsonOut; // TODO: determine what all is being sent back
+
+/* Declaring constants that will not be changing throughout the program */
+// First sensor, polling one that goes on the front of the bot
+const int S1_TRIG = 9;
+const int S1_ECHO = 3;
+// Second sensor, polling one that goes on the right side of the bot
+const int S2_TRIG = 5;
+const int S2_ECHO = 4;
+// Third sensor, polling one that goes on the left side of the bot
+const int S3_TRIG = 6;
+const int S3_ECHO = 7;
+
+/* Declaring the side sensors */
+// HCSR04 name(trigger pin, echo pin, minimum distance in mm, maximum distance in mm)
+HCSR04 SensorFront(S1_TRIG, S1_ECHO, 25, 4000);
+HCSR04 SensorRight(S2_TRIG, S2_ECHO, 25, 4000);
+HCSR04 SensorLeft(S3_TRIG, S3_ECHO, 25, 4000);
 
 void setup() {
   // Initialize serial port
   Serial.begin(38400); // TODO: decide on Serial rate
-
-  // TODO: set up ultrasonic sensors
-
 
   // TODO: set up magnetometer IMU
   
@@ -17,12 +32,11 @@ void setup() {
 void loop() {
   // Indicate that this Arduino is the sensor Arduino
   jsonOut["sensor"] = 1;
-  
-  // Add data from ultrasonic sensors to JSON
-  // TODO: read data from ultrasonic sensors
-  jsonOut["us_front"] = -1;
-  jsonOut["us_left"] = -1;
-  jsonOut["us_right"] = -1;
+
+  // Read and add data from ultrasonic sensors to JSON
+  jsonOut["us_front"] = SensorFront.distanceInMillimeters();
+  jsonOut["us_left"] = SensorLeft.distanceInMillimeters();
+  jsonOut["us_right"] = SensorRight.distanceInMillimeters();
 
   // Add data from magnetometer to JSON
   // TODO: read data from magnetometer
