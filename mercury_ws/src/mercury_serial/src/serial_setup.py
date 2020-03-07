05a33aa273cfedbad95f12bd551cf27d8a759b70
Ubuntu <codyblack@ou.edu>
2020-03-07 00:02:14 +0000

    Serial setup node now checks first 2 available ttyUSB nodes instead of just 0 and 1

--- mercury_ws/src/mercury_serial/src/serial_setup.py
+++ mercury_ws/src/mercury_serial/src/serial_setup.py
@@ -5,71 +5,75 @@ import json
 import rospy
 import serial
 import time
+import glob
 
 if __name__ == "__main__":
     # Initialize ROS node
     rospy.init_node("serial_setup")
     pub = rospy.Publisher("/mercury/ports", ports, queue_size=1)
+    # Get USB ports that are connected to an Arduino
+    con_ports = glob.glob('/dev/tty[USB]*')
+    # Make sure only two USB ports have an Arduino connected
+    if len(con_ports) is not 2:
+        raise SystemExit("Error: make sure exactly 2 Arduinos are connected")
     port_names = ports()
     # TODO: check ttyUSB0 to ttyUSB4 instead of just ttyUSB0 and ttyUSB1
-    ser_USB0 = serial.Serial(timeout = 1)
-    ser_USB0.baudrate = 38400
-    ser_USB0.port = "/dev/ttyUSB0"
-    ser_USB0.connected = False
-    while not ser_USB0.connected:
-	try:
-	    ser_USB0.open()
-	except Exception as e:
-	    rospy.loginfo_throttle(1, "Error: {}".format(str(e)))
-	else:
-	    ser_USB0.connected = True
-	    time.sleep(0.5)
+    ser_A = serial.Serial(timeout = 1)
+    ser_A.baudrate = 38400
+    ser_A.port = con_ports[0]
+    ser_A.connected = False
+    while not ser_A.connected:
+	    try:
+	        ser_A.open()
+	    except Exception as e:
+	        rospy.loginfo_throttle(1, "Error: {}".format(str(e)))
+	    else:
+	        ser_A.connected = True
+	        time.sleep(0.5)
 
-    ser_USB1 = serial.Serial(timeout = 1)
-    ser_USB1.baudrate = 38400
-    ser_USB1.port = "/dev/ttyUSB1"
-    ser_USB1.connected = False
-
-    while not ser_USB1.connected:
+    ser_B = serial.Serial(timeout = 1)
+    ser_B.baudrate = 38400
+    ser_B.port = con_ports[1]
+    ser_B.connected = False
+    while not ser_B.connected:
         try:
-            ser_USB1.open()
+            ser_B.open()
         except Exception as e:
             rospy.loginfo_throttle(1, "Error: {}".format(str(e)))
         else:
-            ser_USB1.connected = True
+            ser_B.connected = True
             time.sleep(0.5)
 
+    ser_A.read_done = False
+    while not ser_A.read_done:
+	    try:
+	        data_A = json.loads(ser_A.readline().decode())
+	    except Exception as e:
+	        rospy.loginfo_throttle(1, "Error reading from {}: {}".format(ser_A.port, str(e)))
+	    else:
+	        ser_A.close()
+	        ser_A.read_done = True
 
-    ser_USB0.read_done = False
-    while not ser_USB0.read_done:
-	try:
-	    data_USB0 = json.loads(ser_USB0.readline().decode())
-	except Exception as e:
-	    rospy.loginfo_throttle(1, "Error reading from {}: {}".format(ser_USB0.port, str(e)))
-	else:
-	    ser_USB0.close()
-	    ser_USB0.read_done = True
-
-    ser_USB1.read_done = False
-    while not ser_USB1.read_done:
+    ser_B.read_done = False
+    while not ser_B.read_done:
         try:
-            data_USB1 = json.loads(ser_USB1.readline().decode())
+            data_B = json.loads(ser_B.readline().decode())
         except Exception as e:
-            rospy.loginfo_throttle(1, "Error reading from {}: {}".format(ser_USB1.port, str(e)))
+            rospy.loginfo_throttle(1, "Error reading from {}: {}".format(ser_B.port, str(e)))
         else:
-	    ser_USB1.close()
-            ser_USB1.read_done = True
+            ser_B.close()
+            ser_B.read_done = True
 
     rospy.loginfo("Successfully read JSON from both Arduinos")
 
-    if (data_USB0["sensor"] == 0) and (data_USB1["sensor"] == 1):
-	port_names.motor_port = ser_USB0.port
-	port_names.sensor_port = ser_USB1.port
-    elif (data_USB0["sensor"] == 1) and (data_USB1["sensor"] == 0):
-	port_names.sensor_port = ser_USB0.port
-        port_names.motor_port = ser_USB1.port
+    if (data_A["sensor"] == 0) and (data_B["sensor"] == 1):
+	    port_names.motor_port = ser_A.port
+	    port_names.sensor_port = ser_B.port
+    elif (data_A["sensor"] == 1) and (data_B["sensor"] == 0):
+        port_names.sensor_port = ser_A.port
+        port_names.motor_port = ser_B.port
     else:
-	rospy.loginfo("Connected devices are not set up properly")
+	    rospy.loginfo("Connected devices are not set up properly")
 
     rospy.loginfo("Arduino ports: %s", port_names)
     pub.publish(port_names)
